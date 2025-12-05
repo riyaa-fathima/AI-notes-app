946d7576311daa93e6c577536c72b304da88c41a
riyafathima2005 <riyafathima9889@gmail.com>
2025-12-05 12:13:02 +0530

    login api created

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { registerUser, loginUser, getMe } = require("../controller/authController");
+
+const router = express.Router();
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/me",getMe)
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const userRoutes = require("./routes/authRoutes");
+
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
+
+app.use("/user",userRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is running in ${PORT}`));
